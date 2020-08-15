6acf565d0c3446a7e96964daf51aa6b81580cfdb
Daniel Konnek <konnekdaniel@gmail.com>
2020-08-15 20:03:00 +0200

    initial commit

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,14 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   /* here you can define another js file */
   entry: {
-    index: "./src/js/index.js",
-    another: "./src/js/another.js",
+    index: './src/js/index.js',
   },
   output: {
-    filename: "[name].[hash:8].js",
-    path: __dirname + "/dist",
+    filename: '[name].[hash:8].js',
+    path: __dirname + '/dist',
   },
   module: {
     rules: [
@@ -17,27 +16,27 @@ module.exports = {
         test: [/.js$/],
         exclude: /(node_modules)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
+            presets: ['@babel/preset-env'],
           },
         },
       },
       {
         test: /\.html$/i,
-        loader: "html-loader",
+        loader: 'html-loader',
         options: {
           attributes: {
             list: [
               {
-                tag: "img",
-                attribute: "src",
-                type: "src",
+                tag: 'img',
+                attribute: 'src',
+                type: 'src',
               },
               {
-                tag: "img",
-                attribute: "data-gallery-src",
-                type: "src",
+                tag: 'img',
+                attribute: 'data-gallery-src',
+                type: 'src',
               },
             ],
           },
@@ -47,9 +46,9 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)$/i,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "img/[name].[hash:8].[ext]",
+              name: 'img/[name].[hash:8].[ext]',
             },
           },
         ],
@@ -65,11 +64,9 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: "public",
+          from: 'public',
           globOptions: {
-            ignore: [
-              '**/*.DS_Store'
-            ],
+            ignore: ['**/*.DS_Store'],
           },
         },
       ],
@@ -77,16 +74,10 @@ module.exports = {
 
     /* here you can define another html file and its dependencies */
     new HtmlWebpackPlugin({
-      template: "./src/pages/index.html",
+      template: './src/pages/index.html',
       inject: true,
-      chunks: ["index"],
-      filename: "index.html",
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/pages/another.html",
-      inject: true,
-      chunks: ["index", "another"],
-      filename: "another.html",
+      chunks: ['index'],
+      filename: 'index.html',
     }),
   ],
 };
